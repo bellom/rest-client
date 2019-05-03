30d1bb039aa9547b12efd6c8c0a77174c65727ef
bellom <bellomsean@gmail.com>
2019-05-03 03:18:41 +0100

    rails routes added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,25 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should load home as root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title","#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about page" do
-    get static_pages_about_url
+  test "should get about" do
+    get about_path
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', "Contact | #{@base_title}"
   end
