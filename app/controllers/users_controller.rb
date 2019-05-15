a63355e07b847668f01de72e04b15bdad975692a
bellom <bellomsean@gmail.com>
2019-05-15 23:44:51 +0100

    Successful SignUps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #Handle a succesful save
+      flash[:success] = "Welcome to the sample app"
+      redirect_to @user
     else
       render 'new'
     end
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
+                                   :password_confirmation)
     end
 
 end
