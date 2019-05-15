7cd3de1b38423178981615a9d3f934cf4237ea43
bellom <bellomsean@gmail.com>
2019-05-15 13:04:42 +0100

    Debugger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :set_time
+
     def hello
         render html: "hello, world!"
     end
+
+    def set_time
+        @time = Time.now
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    debugger
   end
 
   def new
   end
+
 end
+
