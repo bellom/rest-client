4bb6f9113ad1b6b8a5593ed3e72fb1e8007fc126
bellom <bellomsean@gmail.com>
2019-05-21 00:26:50 +0100

    Finding and authenticating a user

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,17 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log the user in
+    else 
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
