4eac01c59aef35c6e8a7eb45e40ca3e7ba808dad
bellom <bellomsean@gmail.com>
2019-06-09 02:15:52 +0100

    Stats and a follow form

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,16 @@ Rails.application.routes.draw do
   get  '/login',    to: 'session#new'
   post '/login',    to: 'session#create'
   delete '/logout', to: 'session#destroy'
-  
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
 end
